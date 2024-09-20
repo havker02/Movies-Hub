826feb941835859726d45654fe9b65bb42b62a34
havker02 <hg7095947@gmail.com>
2024-09-20 22:33:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const telegram = document.querySelector(".fa-telegram").addEventListener("click"
   window.open("https://telegram.me/thunder_modz");
 });
 
-const btn = document.querySelector(".download").addEventListener("click", function(){
-  window.open("https://github.com/havker02/Movies-Hub/releases/download/v2.1.4n/MoviesHub_Premium.apk","_blank");
-});
-
 document.addEventListener("contextmenu", function (e) {
   e.preventDefault();
+});
+
+const btn = document.querySelector(".download").addEventListener("click", function(){
+  window.open("https://github.com/havker02/Movies-Hub/releases/download/MoviesHub_v2.1.4n/MoviesHub_v2.1.4n.apk","_blank");
 });
