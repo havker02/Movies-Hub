de75376e096deaa85d846553f8f2df800d9dc5d2
havker02 <hg7095947@gmail.com>
2024-09-20 21:35:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const telegram = document.querySelector(".fa-telegram").addEventListener("click", function() {
+  window.open("https://telegram.me/thunder_modz");
+});
+
+const btn = document.querySelector(".download").addEventListener("click", function(){
+  window.open("https://github.com/havker02/Movies-Hub/releases/download/v2.1.4n/MoviesHub_Premium.apk","_blank");
+});
+
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
