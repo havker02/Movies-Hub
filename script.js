a86fb949bd3b324237fa19033de51b92cf4908c3
havker02 <hg7095947@gmail.com>
2025-02-16 00:20:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const download_url = "https://github.com/havker02/Movies-Hub/releases/download/MoviesHub/MOVIES.HUB_2.1.5.apk"
-
 const telegram = document.querySelector(".fa-telegram").addEventListener("click", function() {
   window.open("https://telegram.me/thunder_modz");
 });
@@ -9,5 +7,5 @@ document.addEventListener("contextmenu", function (e) {
 });
 
 const btn = document.querySelector(".download").addEventListener("click", function(){
-  window.open("download_url","_blank");
+  window.open("https://github.com/havker02/Movies-Hub/releases/download/MoviesHub/MOVIES.HUB_2.1.5.apk","_blank");
 });
